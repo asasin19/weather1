5d2a6e1b0a58863411087fb687f1a390fdf90dd3
asasin <asasin19@mail.ru>
2016-08-30 09:26:45 +0300

    -fixed icon in Service -fixed notification description text in Settings -fixed notification turnOn and turnOff -outed part of PictureRenderer to ImageLoader -fixed some small bugs -fixed return saved state to editText in MainActivity -fixed some constant

---
+++ app/src/main/java/com/example/gleb/first/ImageLoader.java
@@ -0,0 +1,33 @@
+package com.example.gleb.first;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.example.gleb.first.cache.Cacher;
+
+import java.net.URL;
+
+/**
+ * Created by Gleb on 28.08.2016.
+ */
+public class ImageLoader {
+    public static final String IMAGE_SOURCE_URI = "http://openweathermap.org/img/w/";
+    public static final String IMAGE_SOURCE_TYPE = ".png";
+
+    public Bitmap getBitmap(String imageName){
+        Bitmap bitmap = Cacher.readImage(imageName);
+        if(bitmap == null){
+            String s_url =  IMAGE_SOURCE_URI + imageName + IMAGE_SOURCE_TYPE;
+            try {
+
+                bitmap = BitmapFactory.decodeStream(new URL(s_url).openStream());
+                Cacher.cacheImage(bitmap, imageName);
+
+            }catch (Exception ex){
+
+            }
+
+        }
+        return bitmap;
+    }
+}
